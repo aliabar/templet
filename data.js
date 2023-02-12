b5b0e9d8a0410e58dbebd53812bea33a1fcb4be1
aliabar <114748399+aliabar@users.noreply.github.com>
2023-02-12 05:48:17 -0800

    zx

--- data.js
+++ data.js
@@ -4,41 +4,41 @@ const dsf=[
         titrl:"Flowers daisy pink stick",
         pris:"$80.00",
         ded:"$90.00",
-        img:"/imges/1.webp"
+        img:"./imges/1.webp"
     },
     {
         id:2,
         titrl:"Jasmine flowers white",
         pris:"$60.00",
         ded:"$70.00",
-        img:"/imges/2.webp"
+        img:"./imges/2.webp"
     },
     {
         id:3,
         titrl:"Blossom bouquet flower",
         pris:"$5.00",
         ded:"$10.00",
-        img:"/imges/7.webp"
+        img:"./imges/7.webp"
     },
     {
         id:4,
         titrl:"Orchid flower red stick",
         pris:"$100.00",
         ded:"$120.00",
-        img:"/imges/2.webp"
+        img:"./imges/2.webp"
     },
     {
         id:5,
         titrl:"Glory of the Snow",
         pris:"$40.00",
         ded:"$50.00",
-        img:"/imges/3.webp"
+        img:"./imges/3.webp"
     },
     {
         id:6,
         titrl:"Rose bouquet white",
         pris:"80.00",
         ded:"$90.00",
-        img:"/imges/5.webp"
+        img:"./imges/5.webp"
     },
 ]
